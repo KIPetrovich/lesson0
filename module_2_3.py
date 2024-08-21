b9d7493836202cb8135ab3586cd797c71085d4e3
KIPetrovich <kazarinovip@mail.ru>
2024-08-22 00:21:53 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,12 +1,12 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 starting_position = 0
 while starting_position < len(my_list):
+    while 0 in my_list:
+        my_list.remove(0)
     if my_list[starting_position] < 0:
         break
-    if my_list[starting_position] > 0:
+    if my_list[starting_position] >= 0:
         print(my_list[starting_position])
         starting_position = starting_position + 1
     else:
         break
-
-
