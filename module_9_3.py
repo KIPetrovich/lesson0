793daa182b7f66fffa1b91f6499010f07303919e
KIPetrovich <kazarinovip@mail.ru>
2024-10-23 00:14:00 +0300

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,15 @@
+
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+
+first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x) != len(y))
+
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first) and len(second)))
+
+
+
+
+
+print(list(first_result))
+print(list(second_result))
